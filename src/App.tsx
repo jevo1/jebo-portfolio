c86dbb67db0b532f47a6f94a24f21710df86a84c
Ark Jeremy Cabras <jevocabras@gmail.com>
2025-09-30 23:07:04 +0800

    [hotfix] source image

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ export default function App() {
               <div className="relative w-32 h-32 md:w-40 md:h-40 rounded-full bg-gradient-to-br from-blue-400 to-purple-500 p-1">
                 <div className="w-full h-full rounded-full bg-slate-800 flex items-center justify-center overflow-hidden">
                     <img
-                      src="src/public/jebo.png"
+                      src="/jebo.png"
                       alt="Profile"
                       className="w-24 h-24 md:w-32 md:h-32 rounded-full object-cover"
                     />
